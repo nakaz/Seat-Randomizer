a37c219b523ec31981704fc68e78cc888e9d19e1
Sean Nakamura <seanenakamura@gmail.com>
2015-07-11 02:00:08 -1000

    make the page pretty

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-  $('#nameInput').append('<input id="studentcount" placeholder=" X">');
-  $('#nameInput').append('<button id="newInput">Number of Students</button>');
+  $('#nameInput').prepend('<button id="newInput">Number of Students</button>');
+  $('#nameInput').prepend('<input id="studentcount" placeholder=" X">');
   $('#newInput').click(function(){
+    $('#listInputs').empty();
     var number = $('#studentcount').val();
     console.log(number);
     for (var i = 0; i < number; i++){
-      $('#nameInput').append('<div class="clear"></div>');
-      $('#nameInput').append('<input class="name">');
+      $('#listInputs').append('<div class="clear"></div>');
+      $('#listInputs').append('<input class="name">');
     }
   });
 
@@ -23,7 +24,7 @@ $(document).ready(function(){
     names = randomSeat(names);
     $(names).each(function(index, element){
       // $('#outputArea').prepend('<li class="someName">' + element + '</li>');
-      $('#' + (index+1)).append('<p>' + element + '</p>');
+      $('#' + (index+1)).append('<p class="seatname">' + element + '</p>');
 
     });
   });
